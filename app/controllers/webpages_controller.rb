bcc75e7e6dda8548cd117d9235a7e11328f0d1c8
carl sednaoui <carlsednaoui@gmail.com>
2012-05-04 17:41:30 -0400

    foo

--- app/controllers/webpages_controller.rb
+++ app/controllers/webpages_controller.rb
@@ -14,6 +14,7 @@ class WebpagesController < ApplicationController
   # GET /webpages/1.json
   def show
     @webpage = Webpage.find(params[:id])
+    render :layout => false and return
 
     respond_to do |format|
       format.html # show.html.erb
